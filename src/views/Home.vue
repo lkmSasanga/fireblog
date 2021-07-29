a371e9f7d3ad795f21ba7096f51659c6f3bf8614
MalinduSasanga <lkmsasanga@gmail.com>
2021-07-29 11:30:10 +0530

    blog posts dummy data added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,5 +6,28 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+      welcomeScreen: {
+        title: "Welcome",
+        blogPost:
+          "Weekly blog blog articles with all things programming including HTML, CSS, JavaScript and more.  Register toady to never miss a post.",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      sampleBlogPost: [
+        {
+          title: "This is a Filter Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "beautiful-stories",
+        },
+        {
+          title: "This is a Filter Title!",
+          blogHTML: "This is a filter blog post title!",
+          blogCoverPhoto: "designed-for-everyone",
+        },
+      ],
+    };
+  },
 };
 </script>
