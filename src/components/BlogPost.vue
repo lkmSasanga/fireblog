b800d209462638d01b2f21603d1a34f2bf0cd350
MalinduSasanga <lkmsasanga@gmail.com>
2021-07-29 20:47:55 +0530

    blogposts page added with styles

--- src/components/BlogPost.vue
+++ src/components/BlogPost.vue
@@ -67,7 +67,77 @@ export default {
     }
 
     div {
-        max-width: 375px;
+      max-width: 375px;
+      padding: 72px 24px;
+      @media (min-width: 700px) {
+        padding: 0 24px;
+      }
+
+      h2 {
+        font-size: 32px;
+        font-weight: 300;
+        text-transform: uppercase;
+        margin-bottom: 24px;
+        @media (min-width: 700px) {
+          font-weight: 40px;
+        }
+      }
+
+      p {
+        font-size: 15px;
+        font-weight: 300;
+        line-height: 1.7;
+      }
+
+      .content-preview {
+        font-size: 13px;
+        max-height: 24px;
+        width: 250px;
+        font-size: 13px;
+        max-height: 24px;
+        white-space: nowrap;
+      }
+
+      .link {
+        display: inline-flex;
+        align-items: center;
+        margin-top: 32px;
+        padding-bottom: 4px;
+        border-bottom: 1px solid transparent;
+        transition: .5s ease-in all;
+
+        &:hover {
+          border-block-color: #303030;
+        }
+      }
+
+      .link-light {
+        &:hover {
+          border-block-color: #ffff;
+        }
+      }
+    }
+  }
+
+  .blog-photo {
+    order: 1;
+    flex: 3;
+    box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+
+    @media (min-width: 700px) {
+      order: 4;
+    }
+
+    @media (min-width: 800px) {
+      order: 2;
+    }
+
+    img {
+      display: block;
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
     }
   }
 }
