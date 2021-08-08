f9912749741bff970791fdd01962c35dfe555fac
MalinduSasanga <lkmsasanga@gmail.com>
2021-08-08 21:09:17 +0530

    forgot-password added

--- src/views/ForgotPassword.vue
+++ src/views/ForgotPassword.vue
@@ -1,9 +1,60 @@
 <template>
-  <div></div>
+  <div class="reset-password">
+    <div class="form-wrap">
+      <form class="reset">
+        <p class="login-register">
+          Back to
+          <router-link class="router-link" :to="{ name: 'Login' }"
+            >Login</router-link
+          >
+        </p>
+        <h2>Reset Password</h2>
+        <p>Forgot your passowrd? Enter your email to reset it</p>
+        <div class="inputs">
+          <div class="input">
+            <input type="text" placeholder="Email" v-model="email" />
+            <email class="icon" />
+          </div>
+        </div>
+        <button @click.prevent="resetPassword">Reset</button>
+        <div class="angle"></div>
+      </form>
+      <div class="background"></div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import email from "../assets/Icons/envelope-regular.svg";
+export default {
+  name: "ForgotPassword",
+  data() {
+    return {
+      email: "",
+      modalActive: false,
+      modalMessage: "",
+      loading: null,
+    };
+  },
+  components: {
+    email,
+  },
+};
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+.reset-password {
+  position: relative;
+  .form-wrap {
+    .reset {
+      h2 {
+        margin-bottom: 8px;
+      }
+      p {
+        text-align: center;
+        margin-bottom: 32px;
+      }
+    }
+  }
+}
+</style>
